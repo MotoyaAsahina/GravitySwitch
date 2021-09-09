4627ee959c4457493ef27cc5658dfd2dba3e33d4
MotoyaAsahina <motoyaasahina@icloud.com>
2021-09-10 04:21:26 +0900

    v2: コース生成方法変更

--- Assets/Scripts/CoinHandler.cs
+++ Assets/Scripts/CoinHandler.cs
@@ -1,30 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinHandler : MonoBehaviour
 {
     public float[] rotation = new float[3];
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(rotation[0], rotation[1], rotation[2]) * Time.deltaTime, Space.World);
+
+        if (!Values.IsPlaying) return;
+
+        transform.position += new Vector3(-Values.Speed, 0, 0) * Time.deltaTime;
+
+        if (gameObject.transform.position.x < Values.EndX)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && Values.IsPlaying)
         {
             Destroy(gameObject);
-            Values.score += 5;
-            other.gameObject.GetComponent<PlayerController>().scoreText.text = Values.score.ToString();
+            Values.Score += 5;
+            other.gameObject.GetComponent<PlayerController>().scoreText.text = Values.Score.ToString();
         }
     }
 }
--- Assets/Scripts/Values.cs
+++ Assets/Scripts/Values.cs
@@ -1,8 +1,27 @@
+using UnityEngine;
+
 public static class Values
 {
-    public static float genSpan;
-    public static bool isPlaying = false;
+    public static ValueNoise1dGenerator Noise1, Noise2;
 
-    public static int score = 0;
-    public static int highScore = 0;
+    public static int BeginX;
+    public static int EndX;
+
+    public static GameObject Box;
+    public static GameObject Coin;
+
+    public static Color[] Color;
+
+    public static int IndexOfBox;
+
+    public static float GenSpan; // old field
+    public static float NormY;
+    public static float Speed;
+
+    public static float CoinRatio;
+
+    public static bool IsPlaying = false;
+
+    public static int Score;
+    public static int HighScore;
 }
